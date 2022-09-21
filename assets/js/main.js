a39b6861df2d12042cb0ba70d29d8e1b57be72cf
ghulamfarid <ghulam.farid@beaconhousetechnology.com>
2022-09-21 15:04:44 +0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const toggleMenuNavigation = (displayState) =>{
+    let nav = document.getElementsByClassName('nav__list')[0];
+    let closeBtn = document.getElementsByClassName('close-icon')[0];
+    if(displayState === 'block'){
+        nav.classList.add('nav__list--active');
+        closeBtn.classList.add('nav__list--active');
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+    }else{
+        nav.classList.remove('nav__list--active');
+        closeBtn.classList.remove('nav__list--active');
+        document.getElementsByTagName('body')[0].style.overflow = 'scroll';
+    }
+}
